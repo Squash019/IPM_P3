bb1168ddd5eb8af38e1862d1b4c6bec12fa85669
Manu <squash019@gmail.com>
2016-12-23 14:57:18 +0100

    Added some new teams to the javascript

--- js/main.js
+++ js/main.js
@@ -35,9 +35,29 @@ document.addEventListener("DOMContentLoaded", function loadLeagues() {
         liceob[i].innerHTML="H.C. LICEO B";
     }
 
-    var borbolla = document.getElementsByClassName("borbolla-a");
-    for (i=0; i<borbolla.length; i++) {
-        borbolla[i].innerHTML="H.C. BORBOLLA A";
+    var borbollaa = document.getElementsByClassName("borbolla-a");
+    for (i=0; i<borbollaa.length; i++) {
+        borbollaa[i].innerHTML="H.C. BORBOLLA A";
+    }
+
+    var borbollab = document.getElementsByClassName("borbolla-b");
+    for (i=0; i<borbollab.length; i++) {
+        borbollab[i].innerHTML="H.C. BORBOLLA B";
+    }
+
+    var compaa = document.getElementsByClassName("compa-a");
+    for (i=0; i<compaa.length; i++) {
+        compaa[i].innerHTML="CLUB COMPAÑÍA DE MARÍA A";
+    }
+
+    var compab = document.getElementsByClassName("compa-b");
+    for (i=0; i<compab.length; i++) {
+        compab[i].innerHTML="CLUB COMPAÑÍA DE MARÍA B";
+    }
+
+    var compostela = document.getElementsByClassName("compostela");
+    for (i=0; i<compostela.length; i++) {
+        compostela[i].innerHTML="C.H. COMPOSTELA";
     }
 
     var dominicos = document.getElementsByClassName("dominicos");
@@ -47,21 +67,19 @@ document.addEventListener("DOMContentLoaded", function loadLeagues() {
 
     /* Cargar logos */
 
-    var liceo = document.getElementsByClassName("logo-liceo");
-    var logo = document.createElement("logo");
-    logo.src="../img/logos/logo-liceo.png";
-    for (i=0; i<liceo.length; i++) {
-        liceo[i].innerHTML="<img src='./img/logos/logo-liceo.png' style='width: 35px'/>";
+    var logo_liceo = document.getElementsByClassName("logo-liceo");
+    for (i=0; i<logo_liceo.length; i++) {
+        logo_liceo[i].innerHTML="<img src='./img/logos/logo-liceo.png' style='width: 35px'/>";
     }
 
-    var borbolla = document.getElementsByClassName("logo-borbolla");
-    for (i=0; i<borbolla.length; i++) {
-        borbolla[i].innerHTML="<img src='./img/logos/logo-borbolla.png' style='width: 35px'/>";
+    var logo_borbolla = document.getElementsByClassName("logo-borbolla");
+    for (i=0; i<logo_borbolla.length; i++) {
+        logo_borbolla[i].innerHTML="<img src='./img/logos/logo-borbolla.png' style='width: 35px'/>";
     }
 
-    var dominicos = document.getElementsByClassName("logo-dominicos");
-    for (i=0; i<dominicos.length; i++) {
-        dominicos[i].innerHTML="<img src='./img/logos/logo-dominicos.png' style='width: 35px'/>";
+    var logo_dominicos = document.getElementsByClassName("logo-dominicos");
+    for (i=0; i<logo_dominicos.length; i++) {
+        logo_dominicos[i].innerHTML="<img src='./img/logos/logo-dominicos.png' style='width: 35px'/>";
     }
 
 });
